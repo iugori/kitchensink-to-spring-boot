32b56a913e723248bf030419d25d41036de3090e
iugori <iulian.goriac@gmail.com>
2024-09-27 20:56:37 +0300

    visual clone

--- ks-front/src/app/app.component.ts
+++ ks-front/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IMember } from './entities/member.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'ks-front';
+  title = "Welcome to JBoss!";
+  members: IMember[] = [
+    {
+      id: 0,
+      name: "John Smith",
+      email: "john.smith@mailinator.com",
+      phoneNumber: "2125551212",
+    }
+  ];
+
+  trackById(index: number, item: IMember): number {
+    return item.id;
+  }
+
 }
