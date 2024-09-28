e64686587340909f7f4faf06fc0e0616a4b17c01
iugori <iulian.goriac@gmail.com>
2024-09-28 12:29:59 +0300

    first functional front-end

--- ks-front/src/app/app.component.ts
+++ ks-front/src/app/app.component.ts
@@ -1,24 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { IMember } from './entities/member.model';
 import { CommonModule } from '@angular/common';
 import { NgForm, FormsModule } from '@angular/forms';
 import { MemberService } from './services/member.service';
 import { HttpClientModule } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule,RouterOutlet],
+  imports: [CommonModule, FormsModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  restRoot = MemberService.URL;
   title = "Welcome to JBoss!";
 
-  restRoot = MemberService.URL;
-
   members: IMember[] = [];
+  messages: string[] = [];
+
+  @ViewChild('regForm') regForm!: NgForm;
 
   constructor(private memberService: MemberService) { }
 
@@ -34,9 +37,22 @@ export class AppComponent {
     this.memberService.getMembers().subscribe(members => this.members = members);
   }
 
-  registerMember(regForm: NgForm) {
-    const formData = { ...regForm.value, reg: "reg" };
-    console.log(formData);
+  registerMember() {
+    console.log(this.regForm);
+    const member = { ...this.regForm.value /*, reg: "reg" */ };
+    this.memberService.addMember(member)
+      .subscribe(r => {
+        if (r) {
+          this.messages = [];
+          for (const key in r) {
+            this.messages.push(`${key}: ${r[key]}`);
+          }
+        } else {
+          this.regForm.resetForm();
+          this.messages = ["Registered!"];
+          this.getMembers();
+        }
+      });
   }
 
 }
--- ks-front/src/app/services/member.service.ts
+++ ks-front/src/app/services/member.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { IMember } from '../entities/member.model';
 
 @Injectable({
@@ -10,25 +10,21 @@ export class MemberService {
 
   public static URL = "/rest/members";
 
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   constructor(private http: HttpClient) { }
 
   getMembers(): Observable<IMember[]> {
-    const members = of([
-      {
-        id: 0,
-        name: "John Smith",
-        email: "john.smith@mailinator.com",
-        phoneNumber: "2125551212",
-      },
-      {
-        id: 1,
-        name: "John Smith",
-        email: "john.smith@mailinator.com",
-        phoneNumber: "2125551212",
-      }
-    ]);
     return this.http.get<IMember[]>(MemberService.URL);
-    // return this.http.get<IMember[]>(MemberService.URL);
+  }
+
+  addMember(member: IMember): Observable<any> {
+    return this.http.post(MemberService.URL, member, this.httpOptions)
+      .pipe(
+        catchError((error: HttpErrorResponse) => of(error.error))
+      )
   }
 
 }
